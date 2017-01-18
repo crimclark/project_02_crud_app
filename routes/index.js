13e509cdad603fd600cc1172404017263e01f36e
Tim Clark <oeomusic@gmail.com>
2017-01-17 16:32:08 -0800

    sends to mongo

--- routes/index.js
+++ routes/index.js
@@ -2,20 +2,14 @@ var express = require('express');
 var router = express.Router();
 var mongo = require('mongodb').MongoClient;
 var objectId = require('mongodb').ObjectID;
-// var passport = require('passport');
-// var localStrategy = require('passport-local').Strategy;
-
-// app.use(passport.initialize());
-// app.use(passport.session());
 
 
 var url = process.env.MONGODB_URI || 'mongodb://localhost:27017/crudapp';
 
 var multer = require('multer');
 var multerS3 = require('multer-s3');
+
 var aws = require('aws-sdk');
-
-
 aws.config.update({
   secretAccessKey: 'OSf2QS5mePwtzUzBFG+9/Pat56ON5kSIJjaf66kV',
   accessKeyId: 'AKIAJP64EUGYNTXP32CQ',
@@ -37,19 +31,38 @@ var upload = multer({
     })
 });
 
-router.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
+// router.get('/', function(req, res){
+//   res.sendFile(__dirname + '/index.html');
+// });
 
 router.post('/upload', upload.array('upl',1), function (req, res, next) {
-    var filename = req.files[0].originalname;
-    var fileLocation = req.files[0].location;
-    console.log(req.files);
-    res.send('Successfully uploaded ' + '<a href=\"' + fileLocation + '\">' + filename + '</a>');
+  var file = {
+    name: req.files[0].originalname,
+    location: req.files[0].location
+  }
+  mongo.connect(url, function(err, db){
+    db.collection('files').insertOne(file, function(err, result){
+      db.close();
+      res.redirect('/');
+    })
+  })
+    // var filename = req.files[0].originalname;
+    // var fileLocation = req.files[0].location;
+    // console.log(req.files);
+    // res.send('Successfully uploaded ' + '<a href=\"' + fileLocation + '\">' + filename + '</a>');
 });
 
 // https://github.com/zishon89us/node-cheat/blob/master/aws/express_multer_s3/app.js
 
+router.get('/', function(req, res){
+  mongo.connect(url, function(err, db){
+    db.collection('files').find().toArray(function(err, docs){
+      console.log(docs);
+      db.close();
+      res.render('index', {files: docs});
+    })
+  })
+})
 
 
 module.exports = router;
