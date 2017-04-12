7adf05389444823e1cf2cdeb8bd6928724b13f3b
Tim Clark <oeomusic@gmail.com>
2017-04-12 15:45:36 -0700

    refactors to all routes to mongoose and moves helper functions to lib folder

--- config/aws.js
+++ config/aws.js
@@ -1,16 +1,16 @@
-var multer = require('multer');
-var multerS3 = require('multer-s3');
+const multer = require('multer');
+const multerS3 = require('multer-s3');
 
-var aws = require('aws-sdk');
+const aws = require('aws-sdk');
 aws.config.update({
   secretAccessKey: process.env.AWS_KEY,
   accessKeyId: process.env.ACCESS_ID,
   region: 'us-east-1'
 });
 
-var s3 = new aws.S3();
+const s3 = new aws.S3();
 
-var upload = multer({
+const upload = multer({
   storage: multerS3({
     s3: s3,
     acl: 'public-read',
@@ -23,20 +23,8 @@ var upload = multer({
   })
 });
 
-function deleteFromAWS(key) {
-  var s3 = new aws.S3();
-  var params = {
-    Bucket: 'gacrudapp',
-    Key: key
-  }
-  s3.deleteObject(params, function(err, data){
-    if (err) console.log(err, err.stack);
-    else console.log(data);
-  })
-}
+module.exports = upload;
 
-
-module.exports = {
-  upload: upload,
-  delete: deleteFromAWS
-}
+// module.exports = {
+//   upload: upload
+// }
--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const mongo = require('mongodb').MongoClient;
-const objectId = require('mongodb').ObjectID;
-const aws = require('../config/aws');
-
-var url = process.env.MONGODB_URI || 'mongodb://localhost:27017/crudapp';
-
+const upload = require('../config/aws');
 const File = require('../models/File');
-
-
+const parseTags = require('../lib/parseTags');
+const deleteFromAWS = require('../lib/deleteFromAWS');
 
 router.post('/files/:key/delete', function(req, res){
   var key = req.body.key;
-  aws.delete(key);
-  mongo.connect(url, (err, db) => {
-    db.collection('files').deleteOne({key: key}, function(res){
-        db.close();
-      });
+  deleteFromAWS(key);
+  File.deleteOne({key: key}, (err, file) => {
     res.redirect('/');
   });
 });
@@ -24,25 +16,14 @@ router.post('/files/:key/delete', function(req, res){
 router.post('/files/:key/update', function(req, res){
   var key = req.body.key;
   var description = req.body.description;
-  mongo.connect(url, (err, db) => {
-    db.collection('files').update({key: key}, {$set: {description: description}});
-    db.close();
-  });
-  res.json({status: 200});
+  File.update({key: key}, {$set: {description: description}}, (err, file) => {
+    res.json({status: 200});
+  })
 });
 
-function parseTags(tags){
-  if (tags) {
-    var tagsArr = tags.split(',');
-    return tagsArr;
-  }
-  return;
-}
-
-router.post('/upload', aws.upload.array('upl',1), function (req, res, next) {
+router.post('/upload', upload.array('upl',1), function (req, res, next) {
   var tags = req.body.tags;
-  console.log('tags:' + tags);
-  var file = {
+  const fileUpload = new File({
     user: req.body.name,
     name: req.files[0].originalname,
     location: req.files[0].location,
@@ -50,35 +31,28 @@ router.post('/upload', aws.upload.array('upl',1), function (req, res, next) {
     tags: parseTags(tags),
     type: req.files[0].mimetype,
     key: req.files[0].key
-  }
-  mongo.connect(url, function(err, db){
-    db.collection('files').insertOne(file, function(err, result){
-      db.close();
-      res.redirect('/');
-    })
   })
+  fileUpload.save( (err, upload) => {
+    res.redirect('/');
+  });
 });
 
 // https://github.com/zishon89us/node-cheat/blob/master/aws/express_multer_s3/app.js
 
 router.get('/tag/:tag', function(req, res){
   var tag = req.params.tag;
-  mongo.connect(url, function(err, db){
-    db.collection('files').find({tags: tag}).toArray(function(err, docs){
-      db.close();
-      res.render('tagfilter', {files: docs, tag: tag});
-    })
-  })
+  File.find({tags: tag}, (err, files) => {
+    if (err) throw err;
+    res.render('tagfilter', {files: files, tag: tag});
+  });
 });
 
 router.get('/users/:user', function(req, res){
   var user = req.params.user;
-  mongo.connect(url, function(err, db){
-    db.collection('files').find({user: user}).toArray(function(err, docs){
-      db.close();
-      res.render('userfilter', {files: docs});
-    })
-  })
+  File.find({user: user}, (err, files) => {
+    if (err) throw err;
+    res.render('userfilter', {files: files});
+  });
 });
 
 router.get('/form', function(req, res){
@@ -96,13 +70,11 @@ router.get('/data', function(req, res){
   res.sendFile('./public/data.html' , { root : './'});
 })
 
-router.get('/data.json', function(req, res){
-  mongo.connect(url, function(err, db){
-    db.collection('files').find().toArray(function(err, docs){
-      db.close();
-      res.json(docs);
-    })
-  });
+router.get('/data.json', (req, res) =>{
+  File.find( (err, files) => {
+    if (err) throw err;
+    res.json(files);
+  })
 })
 
 module.exports = router;
